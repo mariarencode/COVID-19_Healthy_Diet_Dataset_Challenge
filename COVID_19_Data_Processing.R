562237d1d387659b19b7cdbfcf9742509017996e
Maria Ren <mariakexinren@gmail.com>
2020-10-20 19:07:35 -0400

    Update COVID-19 data to 10/19/20.

--- COVID_19_Data_Processing.R
+++ COVID_19_Data_Processing.R
@@ -7,8 +7,8 @@ library(data.table)
 
 # Daily Automatic Update Links
 
-# covid_19_link currently updated to 10/11/2020
-covid_19_link <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/10-11-2020.csv"
+# covid_19_link currently updated to 10/19/2020
+covid_19_link <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/10-19-2020.csv"
 population_link <- "https://datacenter.prb.org/download/international/indicator/population/csv"
 
 
